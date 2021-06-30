3f544d816326c9c4cb0d2a622e287fcc357ed888
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-06-30 12:11:05 +0100

    Add first test and method for inputting names and birthdays

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,18 @@
+class BirthdayList
+
+  def initialize
+    @list =[]
+  end
+
+  def add(name, date)
+    @list << { name: name, birthday: date }
+  end
+
+  def shows_list
+  end
+
+  def check_birthday
+  end
+  
+end
 
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,9 @@
+require 'birthday_list'
 
+describe BirthdayList do
+  it 'adds birthday' do
+    birthday_list = BirthdayList.new
+    expect(birthday_list.add('Tom Jones', '17 December 2000')).to eq [{name: 'Tom Jones', birthday: '17 December 2000'} ]
+ end
+
+end
